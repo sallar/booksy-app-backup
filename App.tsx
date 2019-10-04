53c8dc2fc5d87b8095e4af3a3cea643c00403b13
Sallar Kaboli <sallar@kaboli.org>
2019-10-04 21:22:29 +0300

    Add minimal bottom bar navigation

--- App.tsx
+++ App.tsx
@@ -1,19 +1,78 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//     </View>
+//   );
+// }
+
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Home</Text>
     </View>
   );
-}
+};
+
+const MyBooksScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>My Books</Text>
+    </View>
+  );
+};
+
+const SettingsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Settings</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: HomeScreen,
+    "My Books": MyBooksScreen,
+    Settings: SettingsScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "Home") {
+          iconName = `ios-search`;
+        } else if (routeName === "Settings") {
+          iconName = `ios-options`;
+        } else if (routeName === "My Books") {
+          iconName = `ios-book`;
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray"
+    }
+  }
+);
+
+export default createAppContainer(TabNavigator);
